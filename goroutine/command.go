240efb26eff4fa9cb01ea2b5883b6643c726f0cc
yang yang <yangyang@yangdeMacBook-Pro.local>
2018-10-19 11:49:13 +0800

    update

--- goroutine/command.go
+++ goroutine/command.go
@@ -2,41 +2,23 @@ package main
 
 import (
 	"fmt"
-	"time"
-	"sync/atomic"
+	// "time"
+	// "sync/atomic"
 )
 
 func main()  {
 	num := uint32(10)
-	var count uint32
-
-	trigger := func(i uint32, fn func())  {
-		for {
-			if n := atomic.LoadUint32(&count); n == i {
-				fn()
-				atomic.AddUint32(&count, 1)
-				break
-			}
-			time.Sleep(time.Nanosecond)
-		}
-	}
+	sign := make(chan struct{})
 
 	for i := uint32(0); i < num; i ++ {
 		go func(i uint32)  {
-			// fmt.Println(i)
-			// sign <- struct{}{}
-			fn := func() {
-				fmt.Println(i)
-			}
-			trigger(i, fn)
+			fmt.Println(i)
+			sign <- struct{}{}
 		}(i)
 	}
 	// time.Sleep(time.Millisecond * 500)
-	// for j := 0; j < num; j ++ {
-	// 	<-sign
-	// }
-	
-
-	trigger(10, func() {})
+	for j := uint32(0); j < num; j ++ {
+		<-sign
+	}
 }
 
